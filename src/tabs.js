8ab7d8bc959d299502cfcc4c81ada31f4c090b0b
YanChang <yan.chang@striking.ly>
2017-04-26 22:49:15 +0800

    add nav ba

--- src/tabs.js
+++ src/tabs.js
@@ -1,25 +1,11 @@
 import $ from 'jquery'
 
-function addBtnHandler(btn) {
-  btn.click((event) => {
-    $('.tabcontent').hide()
-    $('.tablinks').each(link => {
-      $(link).removeClass('active')
-    })
-    $('.tabcontent').each(content => {
-      $(content).removeClass('active')
-    })
-
-    debugger
-    const shouldOpenId = $(this).attr('id').toLowerCase()
-    $(`#${shouldOpenId}`).show().addClass('active')
-    $(this).addClass('active')
-  })
-}
-
 export default function setUpTabsHandlers() {
-  $('.tablinks').each(btn => {
-    debugger
-    addBtnHandler($(this))
+  $('.tablink').click(function(ev) {
+    $('.tabcontent').hide()
+    $('.tablink').removeClass('active')
+
+    $('.tabcontent').eq($(this).index()).show()
+    $('.tablink').eq($(this).index()).addClass('active')
   })
 }
