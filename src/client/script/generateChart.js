1d01ff05970bc1512bb7e11d0ea71ac4ed62c6fc
thomasyimgit <thomasyim94@gmail.com>
2017-04-28 22:29:11 +0800

    update jade template && express jade middleware

--- src/client/script/generateChart.js
+++ src/client/script/generateChart.js
@@ -1,13 +1,13 @@
 import 'echarts-liquidfill'
 import echarts from 'echarts'
-import china from './china'
 import $ from 'jquery'
 
+import './echarts/china'
 // invalid dom right now, try container
 // const liquidfill = echarts.init(document.querySelector('.liquidfill'))
 
 export function makeLiquidfill(percentage = 0) {
-  if(!$('.liquidfill').is(":visible")) {
+  if (!$('.liquidfill').is(':visible')) {
     $('.liquidfill').show()
   }
 
@@ -18,7 +18,7 @@ export function makeLiquidfill(percentage = 0) {
       data: [percentage, percentage - 0.1, percentage - 0.2],
     }],
   }
-  if(!shouldStop) {
+  if (!shouldStop) {
     liquidfill.setOption(option)
     setTimeout(() => makeLiquidfill(percentage + 0.1), 700)
   }
@@ -37,95 +37,93 @@ export function makePm25Chart(data) {
       sublink: 'http://www.pm25.in',
       left: 'center',
       textStyle: {
-          color: '#fff'
-      }
+        color: '#fff',
+      },
     },
-    tooltip : {
-      trigger: 'item'
+    tooltip: {
+      trigger: 'item',
     },
     legend: {
       orient: 'vertical',
       y: 'bottom',
-      x:'right',
-      data:['pm2.5'],
+      x: 'right',
+      data: ['pm2.5'],
       textStyle: {
-          color: '#fff'
-      }
+        color: '#fff',
+      },
     },
     geo: {
       map: 'china',
       label: {
         emphasis: {
-          show: false
-        }
+          show: false,
+        },
       },
       roam: true,
       itemStyle: {
         normal: {
           areaColor: '#323c48',
-          borderColor: '#111'
+          borderColor: '#111',
         },
         emphasis: {
-          areaColor: '#2a333d'
-        }
-      }
+          areaColor: '#2a333d',
+        },
+      },
     },
-    series : [
+    series: [
       {
         name: 'pm2.5',
         type: 'scatter',
         coordinateSystem: 'geo',
-        data: data,
-        symbolSize: function (val) {
+        data,
+        symbolSize(val) {
           return val[2] / 10;
         },
         label: {
           normal: {
             formatter: '{b}',
             position: 'right',
-            show: false
+            show: false,
           },
           emphasis: {
-            show: true
-          }
+            show: true,
+          },
         },
         itemStyle: {
           normal: {
-            color: '#ddb926'
-          }
-        }
+            color: '#ddb926',
+          },
+        },
       },
       {
         name: 'Top 5',
         type: 'effectScatter',
         coordinateSystem: 'geo',
-        data: data.sort(function (a, b) {
-          return b.value - a.value;
-        }).slice(0, 6),
-        symbolSize: function (val) {
+        data: data.sort((a, b) => b.value - a.value).slice(0, 6),
+        symbolSize(val) {
           return val[2] / 10;
         },
         showEffectOn: 'render',
         rippleEffect: {
-          brushType: 'stroke'
+          brushType: 'stroke',
         },
         hoverAnimation: true,
         label: {
           normal: {
             formatter: '{b}',
             position: 'right',
-            show: true
-          }
+            show: true,
+          },
         },
         itemStyle: {
           normal: {
             color: '#f4e925',
             shadowBlur: 10,
-            shadowColor: '#333'
-          }
+            shadowColor: '#333',
+          },
         },
-        zlevel: 1
-      }
-    ]
+        zlevel: 1,
+      },
+    ],
   });
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
+/* eslint "no-underscore-dangle": 0 */
 const path = require('path')
 const express = require('express')
-const open = require('open')
 
 const airPollution = require('./data/airPollution')
 const { server } = require('../../config')
@@ -9,19 +9,28 @@ const app = express()
 
 app.use('/dist', express.static('dist'))
 
+app.set('views', path.resolve(__dirname, '../../', './src/client/template'))
+app.set('view engine', 'jade')
+
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './index.html'))
+  res.render('index', {
+    text: 'Hello World',
+  })
 })
 
-app.get('/pm25', (req, res) => {
-  airPollution.get().then((re) => {
-    res.json(re)
-  }).catch(err => err)
+app.get('/air-pollution', (req, res) => {
+  airPollution.get().then((result) => {
+    res.render('air-pollution', {
+      result,
+    })
+  })
+  .catch((err) => {
+    console.error(err)
+  })
 })
 
 app.listen(server.port, () => {
   console.log('server start')
-  open(`http://localhost:${server.port}`)
 })
 
 process.on('uncaughtException', (err) => {
