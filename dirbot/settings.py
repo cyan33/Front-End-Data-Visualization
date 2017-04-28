8a24461a043d169a42c7314fa60aa80525f7201e
loatheb <zz.innj@gmail.com>
2017-04-28 20:56:56 +0800

    reafactor project architecture

--- dirbot/settings.py
+++
@@ -1,7 +0,0 @@
-# Scrapy settings for dirbot project
-
-SPIDER_MODULES = ['dirbot.spiders']
-NEWSPIDER_MODULE = 'dirbot.spiders'
-DEFAULT_ITEM_CLASS = 'dirbot.items.Website'
-
-ITEM_PIPELINES = {'dirbot.pipelines.FilterWordsPipeline': 1}
