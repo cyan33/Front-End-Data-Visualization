ad57d18517277435127abad41b7314246ecbdcff
thomasyimgit <thomasyim94@gmail.com>
2017-05-17 20:45:43 +0800

    use echarts heatmap in place of d3

--- src/client/script/generateChart.js
+++ src/client/script/generateChart.js
@@ -847,11 +847,6 @@ export function generateBudgetProposal() {
   })
 }
 
-// directly send dirty html file from backend
-// export function generateUnemploymentRate() {
-
-// }
-
 export function generateDailyActivityProportion() {
   const cellSize = [80, 80];
   const pieRadius = 30;
@@ -971,4 +966,177 @@ export function generateDailyActivityProportion() {
       }
     };
   }
+}
+
+export function generateHeatMap() {
+  function getVirtualData(year = '2017') {
+    const date = +echarts.number.parseDate(`${year}-01-01`);
+    const end = +echarts.number.parseDate(`${+year + 1}-01-01`);
+    const dayTime = 3600 * 24 * 1000; // seconds * hour * millseconds
+    const data = [];
+    for (let time = date; time < end; time += dayTime) {
+      data.push([
+        echarts.format.formatTime('yyyy-MM-dd', time),
+        Math.floor(Math.random() * 10000),
+      ]);
+    }
+    return data; // [['2017-01-01', 4603], ['2017-01-02', 6785], ...],length === 365
+  }
+
+  const data = getVirtualData('2017')
+
+  hideLoading()
+  resizeHandler()
+
+  myChart.setOption({
+    backgroundColor: '#404a59',
+
+    title: {
+      top: 30,
+      text: 'My Step Count in 2017',
+      subtext: 'Virtual Data',
+      left: 'center',
+      textStyle: {
+        color: '#fff',
+      },
+    },
+    tooltip: {
+      trigger: 'item',
+    },
+    legend: {
+      top: '30',
+      left: '100',
+      data: ['Steps', 'Top 12'],
+      textStyle: {
+        color: '#fff',
+      },
+    },
+    calendar: [{
+      top: 100,
+      left: 'center',
+      range: ['2017-01-01', '2017-06-30'],
+      splitLine: {
+        show: true,
+        lineStyle: {
+          color: '#000',
+          width: 4,
+          type: 'solid',
+        },
+      },
+      yearLabel: {
+        formatter: '{start}  1st',
+        textStyle: {
+          color: '#fff',
+        },
+      },
+      itemStyle: {
+        normal: {
+          color: '#323c48',
+          borderWidth: 1,
+          borderColor: '#111',
+        },
+      },
+    }, {
+      top: 340,
+      left: 'center',
+      range: ['2017-07-01', '2017-12-31'],
+      splitLine: {
+        show: true,
+        lineStyle: {
+          color: '#000',
+          width: 4,
+          type: 'solid',
+        },
+      },
+      yearLabel: {
+        formatter: '{start}  2nd',
+        textStyle: {
+          color: '#fff',
+        },
+      },
+      itemStyle: {
+        normal: {
+          color: '#323c48',
+          borderWidth: 1,
+          borderColor: '#111',
+        },
+      },
+    }],
+    series: [
+      {
+        name: 'Steps',
+        type: 'scatter',
+        coordinateSystem: 'calendar',
+        data,
+        symbolSize(val) {
+          return val[1] / 500;
+        },
+        itemStyle: {
+          normal: {
+            color: '#ddb926',
+          },
+        },
+      },
+      {
+        name: 'Steps',
+        type: 'scatter',
+        coordinateSystem: 'calendar',
+        calendarIndex: 1,
+        data,
+        symbolSize(val) {
+          return val[1] / 500;
+        },
+        itemStyle: {
+          normal: {
+            color: '#ddb926',
+          },
+        },
+      },
+      {
+        name: 'Top 12',
+        type: 'effectScatter',
+        coordinateSystem: 'calendar',
+        calendarIndex: 1,
+        data: data.sort((a, b) => b[1] - a[1]).slice(0, 12),
+        symbolSize(val) {
+          return val[1] / 500;
+        },
+        showEffectOn: 'render',
+        rippleEffect: {
+          brushType: 'stroke',
+        },
+        hoverAnimation: true,
+        itemStyle: {
+          normal: {
+            color: '#f4e925',
+            shadowBlur: 10,
+            shadowColor: '#333',
+          },
+        },
+        zlevel: 1,
+      },
+      {
+        name: 'Top 12',
+        type: 'effectScatter',
+        coordinateSystem: 'calendar',
+        data: data.sort((a, b) => b[1] - a[1]).slice(0, 12),
+        symbolSize(val) {
+          return val[1] / 500;
+        },
+        showEffectOn: 'render',
+        rippleEffect: {
+          brushType: 'stroke',
+        },
+        hoverAnimation: true,
+        itemStyle: {
+          normal: {
+            color: '#f4e925',
+            shadowBlur: 10,
+            shadowColor: '#333',
+          },
+        },
+        zlevel: 1,
+      },
+    ],
+  })
 }
